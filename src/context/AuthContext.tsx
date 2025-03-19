5a1bf796982913d2086fa95e92fad5cecc8b5766
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-19 00:26:31 +0000

    getSession&signout

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { User } from "@supabase/supabase-js";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "../supabase-client";
 
 interface AuthContextType {
@@ -12,10 +12,27 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
   const signInWhithGithub = () => {
     supabase.auth.signInWithOAuth({ provider: "github" });
   };
-  const signOut = () => {};
+  const signOut = () => {
+    supabase.auth.signOut();
+  };
 
   return (
     <AuthContext.Provider value={{ user, signInWhithGithub, signOut }}>
