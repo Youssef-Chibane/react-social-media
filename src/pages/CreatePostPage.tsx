9ce10a4d8524ccef49e928e6e982736497dba07e
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-19 22:15:44 +0000

    CreatePostPage

---
+++ src/pages/CreatePostPage.tsx
@@ -0,0 +1,12 @@
+import CreatePost from "../components/CreatePost";
+
+const CreatePostPage = () => {
+  return (
+    <div>
+      <h1>Create New Post</h1>
+      <CreatePost />
+    </div>
+  );
+};
+
+export default CreatePostPage;
