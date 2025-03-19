3f542fd1c1bdbb5ffd4c1f643e51c6ea5fab5d10
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-19 22:52:50 +0000

    createPost handelSubmit

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
+import { supabase } from "../supabase-client";
+import { useMutation } from "@tanstack/react-query";
+
+interface PostInput {
+  title: string;
+  content: string;
+}
+
+const createPost = async (post: PostInput) => {
+  const { data, error } = await supabase.from("posts").insert(post);
+
+  if (error) throw new Error(error.message);
+
+  return data;
+};
+
 const CreatePost = () => {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const { mutate } = useMutation({ mutationFn: createPost });
+
+  const handelSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    mutate({ title, content });
+  };
   return (
-    <form>
+    <form onSubmit={handelSubmit}>
       <div>
         <label>Title</label>
-        <input type="text" id="title" required />
+        <input
+          type="text"
+          id="title"
+          required
+          onChange={(event) => setTitle(event.target.value)}
+        />
       </div>
       <div>
         <label>Content</label>
-        <textarea id="content" required rows={5} />
+        <textarea
+          id="content"
+          required
+          rows={5}
+          onChange={(event) => setContent(event.target.value)}
+        />
       </div>
       <button type="submit">Create Post</button>
     </form>
