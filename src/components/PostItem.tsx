1324e5a770708a607b81eb60697ec902a569cb5c
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-03-20 23:19:24 +0000

    PostItem

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,15 @@
+import { Post } from "./PostList";
+
+interface Props {
+  post: Post;
+}
+
+const PostItem = ({ post }: Props) => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default PostItem;
